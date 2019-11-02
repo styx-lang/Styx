e785f116b281ebdfb34beff5d906d3f02ab6bb73
Lasse Dissing Hansen <me@lassedissing.com>
2019-11-02 22:38:35 +0100

    Add basic testing suite
    Found and fixes bug meaning that literals larger than 2^31 was
     not properly generated

---
+++ tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p build/testing
+
+for filename in tests/*.expect; do
+  test_name=$(basename "$filename" .expect)
+  echo $test_name
+  source="tests/${test_name}.z"
+  output="build/testing/${test_name}.c"
+  eval "$1" "$source" "$output"
+  gcc "build/testing/${test_name}.c" -o "build/testing/${test_name}"
+  "build/testing/${test_name}" | diff "tests/${test_name}.expect" -
+done
