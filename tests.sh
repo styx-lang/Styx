7c36d5c5ffa503a863cb744914c3d3d9a8e196f4
Lasse Dissing Hansen <me@lassedissing.com>
2019-11-25 21:56:03 +0100

    Add loading of external modules from search path

--- tests.sh
+++ tests.sh
@@ -8,7 +8,7 @@ for filename in tests/*.expect; do
   echo $test_name
   source="tests/${test_name}.z"
   output="build/testing/${test_name}.c"
-  eval "$1" "$source" "$output"
+  eval "$1" "$source" "$output" "lib"
   gcc "build/testing/${test_name}.c" -o "build/testing/${test_name}"
   "build/testing/${test_name}" | diff "tests/${test_name}.expect" -
 done
