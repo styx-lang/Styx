965a4bd24280b34ca91501e959990d878b339cc5
Lasse Dissing Hansen <me@lassedissing.com>
2019-12-05 22:30:30 +0100

    Add preliminary parsing for match expressions

--- unstable_build.sh
+++ unstable_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-time build/stage0 src/main.z "build/unstable.c"
+build/stage0 src/main.z "build/unstable.c"
 
-time gcc -g -Wall build/unstable.c -o build/unstable
+gcc -g -Wall build/unstable.c -o build/unstable
